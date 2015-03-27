fca279752900b9f1ce2d4f2a91d61f14b79fb919
Antti Rasinen <ars@iki.fi>
2015-03-27 22:44:21 +0200

    Print all tracks

--- holux-dl/main.go
+++ holux-dl/main.go
@@ -21,7 +21,14 @@ func main() {
 		log.Fatalf("Got error %v, arborting", err)
 	}
 
-	for _, row := range index {
+	for k, row := range index {
 		fmt.Println(row)
+		track, err := c.GetTrack(row.Offset, row.Size)
+		if err != nil {
+			log.Fatal("Got error %v while reading track %d", err, k)
+		}
+		for _, t := range track {
+			fmt.Println(t)
+		}
 	}
 }
